d597673abe8b48eb26fe06ad330855ebbfa48391
michaelArvidsson <michael.arvidsson@gmail.com>
2020-11-05 10:50:18 +0100

    added loops

--- Get PRescriptionlist.php
+++ Get PRescriptionlist.php
@@ -21,7 +21,7 @@
 
 <body>
   <h1>välkommen till Majornas Vårdcentral</h1>
-<pre>
+  <pre>
   <?php
   ini_set('display_errors', 1);
   ini_set('display_startup_errors', 1);
@@ -95,30 +95,60 @@
   curl_close($ch);
 
 
- /*  $arr_encounter=array();
-  array_push($arr_encounter,["data"]["0"]["name"]); 
-  echo print_r ($arr_encounter); */ 
-//create an array of encounter names
-  echo $response["data"]["0"]["name"];
-  for ($i = 0; $i <= 4; $i++) {
-    
-  foreach ($response["data"] as $key=> $value) {
-
-    foreach ($response["data"]["i"] as $key => $value) {
-      echo '<div>'.$value["name"].'</div>';
-
+  //create an array of encounter names
+  // --- TO DO ---- Fix count length array
+  $arr_encounter = array();
+  for ($i = 0; $i < 5; $i++) {
+    echo $response["data"][$i]["name"];
+    array_push($arr_encounter, $response["data"][$i]["name"]);
   }
-   
+  echo print_r($arr_encounter);
+
+  $encounter = $arr_encounter[1];
+
+  // assign variable url to pull out each encounter
+  $ch = curl_init($baseurl . 'Patient%20Encounter/' . $encounter);
+  curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+
+  curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json', 'Accept: application/json'));
+  curl_setopt($ch, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
+  curl_setopt($ch, CURLOPT_COOKIEJAR, $cookiepath);
+  curl_setopt($ch, CURLOPT_COOKIEFILE, $cookiepath);
+  curl_setopt($ch, CURLOPT_TIMEOUT, $tmeout);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+  $response = curl_exec($ch);
+  $response = json_decode($response, true);
+
+
+  $error_no = curl_errno($ch);
+  $error = curl_error($ch);
+  curl_close($ch);
+
+  echo "<div style='background-color:yellow; border:1px solid black'>";
+  echo 'Result<br><pre>';
+  //echo print_r($response) . "</pre><br>";
+  echo print_r($response['data']['drug_prescription']['0']['drug_name']);
+  // ---- TO DO ------ if loop to check patient
+  // ---- TO DO ------ Create array of all meds use arrayunique then put in dropdown
+  // ---- TO DO ------ create hidden input for sending patient to prescription
+  // Dropdown prescription
+  echo '<form action="Get PRescriptionlist.php" method="POST">';
+  echo '<select name="drug">';
+  // --- TO DO --- Count the length of array drug_prescription set i less than length of array
+  for ($i = 0; $i < 2; $i++) {
+    echo '<option value=' . $response['data']['drug_prescription'][$i]['drug_name'] . ' >' . $response['data']['drug_prescription'][$i]['drug_name'] . '</option>';
   }
-}
+  /*     foreach ($value as $valuekey => $valuevalue) {
+      echo $valuekey . " " . $valuevalue . "<br>";
+      //echo '<option value=' . $valuevalue['drug_name'] . ' >' . $valuevalue['drug_name'] . '</option>';
+    } */
 
+  //echo '<option value=' . $response['data']['drug_prescription']['0']['drug_name'] . ' >' . $response['data']['drug_prescription']['0']['drug_name'] . '</option>';
+  echo '</select>';
+  echo '</form>';
+  echo "</div>";
 
-
-  // $i=0;
-  // foreach ($arr_encounter as $value) {
-  //     $i++;
-  //     echo $i." ".$value."<br>";
-  // }
 
   if (!empty($error_no)) {
     echo "<div style='background-color:red'>";
